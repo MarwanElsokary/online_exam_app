8242bb7d0631458e8b107d1f79da6f40ba10a2a1
shahd523 <shahdehab798@gmail.com>
2025-08-10 20:39:15 +0300

    feat [Home Design]:made exam page view & view model

--- lib/Features/Home/domain/usecases/questions_usecase.dart
+++ lib/Features/Home/domain/usecases/questions_usecase.dart
@@ -8,10 +8,7 @@ import 'check_questions_usecase.dart';
 class QuestionsUsecase {
   QuestionsRepo questionsRepo;
 @factoryMethod
-  QuestionsUsecase(this.questionsRepo);
-  final QuestionsRepo _questionsRepo;
-
-  QuestionsUsecase(this._questionsRepo);
+  QuestionsUsecase(this.questionsRepo );
 
   Future<List<Questions>> invoke({
     required String examId,
